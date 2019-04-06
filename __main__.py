28b09db449bd5817ef11355d8d54e4e4efdc5fab
timn3 <niertim308@gmail.com>
2019-04-06 10:41:31 +0200

    Update

--- __main__.py
+++ __main__.py
@@ -5,10 +5,19 @@ graph = Graph()
 graph.add_node("A")
 graph.add_node("B")
 graph.add_node("C")
+graph.add_node("D")
+graph.add_node("E")
 
-graph.add_edge("A", "B", 1)
-graph.add_edge("B", "C", 1)
-graph.add_edge("A", "C", 3)
+graph.add_edge("A", "B", 2)
+graph.add_edge("D", "E", 1)
+graph.add_edge("D", "C", 1)
+graph.add_edge("E", "C", 1)
+graph.add_edge("E", "D", 1)
+graph.add_edge("C", "CB", 10)
+graph.add_edge("B", "CB", 10)
+graph.add_edge("B", "I", 14)
+graph.add_edge("C", "G", 8)
+
 
 
 path_length, path = graph.find_shortest_path("A", "C")
